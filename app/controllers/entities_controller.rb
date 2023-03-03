fbfb9bf73319c2757d4665e0286bdf38b3efb1c8
Gilbert <okonjogilbert2@gmail.com>
2023-03-03 22:21:06 +0300

    SetUp project with everything

---
+++ app/controllers/entities_controller.rb
@@ -0,0 +1,23 @@
+class EntitiesController < ApplicationController
+  def new
+    @entity = Entity.new
+    @groups = Group.all
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @entity = Entity.new(entity_params)
+    @entity.user = current_user
+    # @entity.group = @group
+
+    return unless @entity.save
+
+    redirect_to user_groups_path(current_user)
+  end
+
+  private
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount, :group_id)
+  end
+end
